6eb3dd0f338a18a20025981a9c33452827a1ec62
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2024-07-30 15:33:49 +0200

    feat: added not found page and side navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeModule } from './home/home.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule],
+  declarations: [AppComponent, NotFoundComponent],
+  imports: [BrowserModule, HomeModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,13 @@ import { HomeComponent } from './home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CatnavigationComponent } from './components/catnavigation/catnavigation.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SidenavigationComponent } from './components/sidenavigation/sidenavigation.component';
+
+
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, CatnavigationComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  declarations: [HomeComponent, HeaderComponent, CatnavigationComponent, SidenavigationComponent],
+  imports: [CommonModule,
+    FontAwesomeModule],
 })
 export class HomeModule {}
