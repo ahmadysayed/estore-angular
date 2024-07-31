8a3ca09e1bbd344fc9f2a273039b9028e8d9ac15
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2024-07-31 10:17:44 +0200

    feat: fetching categories in category navigation bar

--- src/app/home/components/catnavigation/catnavigation.component.ts
+++ src/app/home/components/catnavigation/catnavigation.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../types/category.type';
 
 @Component({
   selector: 'app-catnavigation',
   templateUrl: './catnavigation.component.html',
-  styleUrls: ['./catnavigation.component.scss']
+  styleUrls: ['./catnavigation.component.scss'],
 })
 export class CatnavigationComponent {
-
+  categories: Category[] = [];
+  constructor(categoryService: CategoryService) {
+    categoryService.getAllCategories().subscribe((categories) => {
+      this.categories = categories.filter(
+        (category) => category.parent_category_id == null
+      );
+    });
+  }
 }
