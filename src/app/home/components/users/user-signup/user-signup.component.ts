9e2d0035f925905e2c94ca5db2643a6a94961996
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2024-10-16 16:54:39 +0200

    [VALIDATORS] added form validation

--- src/app/home/components/users/user-signup/user-signup.component.ts
+++ src/app/home/components/users/user-signup/user-signup.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  AbstractControl,
+  Validators,
+} from '@angular/forms';
+
+import { matchPasswords } from './validators/match-password.validator';
 
 @Component({
   selector: 'app-user-signup',
@@ -10,17 +17,38 @@ export class UserSignupComponent implements OnInit {
   userSignupForm: FormGroup;
   constructor(private fb: FormBuilder) {}
   ngOnInit(): void {
-    this.userSignupForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      address: [''],
-      city: [''],
-      state: [''],
-      pin: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
-    });
+    this.userSignupForm = this.fb.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: [''],
+        address: [''],
+        city: [''],
+        state: [''],
+        pin: [''],
+        email: ['', Validators.required, Validators.email],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validators: matchPasswords,
+      }
+    );
+  }
+
+  get firstName(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('firstName');
+  }
+
+  get email(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('email');
+  }
+
+  get password(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('password');
+  }
+
+  get confirmPassword(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('confirmPassword');
   }
 
   onSubmit(): void {}
